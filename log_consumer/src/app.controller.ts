5bf8298f325dd6ec7e595b6619221e5b0dcbc2e0
palinYee <alwns28@kookmin.ac.kr>
2023-10-16 23:04:15 +0900

    docker-compose.yml nestjs build command add

--- log_consumer/src/app.controller.ts
+++ log_consumer/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Ctx,
+  MessagePattern,
+  RmqContext,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('notifications')
+  getNotifications(@Payload() data: number[], @Ctx() context: RmqContext) {
+    console.log(`Pattern: ${context.getPattern()}, data: ${data}`);
   }
 }
